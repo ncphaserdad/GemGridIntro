a21952883184dc22b9c3efdd64b43029f04c3bd6
ncphaserdad <68792633+ncphaserdad@users.noreply.github.com>
2020-08-06 14:48:50 -0400

    change 2

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Phaser from "phaser";
 const stones = ['amber', 'amethyst', 'aquamarine', 'citrine', 'crystal' , 'pyramid'];
 
 const TOP_STONE = 'amber';
+const DEFAULT_STONE ='pyramid';
 const HIDDEN_STONES = ['citrine', 'crystal', 'gem'];
 
 //GEM_SIZE = SCREEN_WIDTH / NUM_X_CELLS;
@@ -45,28 +46,45 @@ function preload ()
 function hideJewels()
 {
   var i;
-  for (i = 0; i < 5; i++) {
-  //Let's get a random number index to pick a stone name from the HIDDEN_STONES array
-  var stoneIndex = rnd.integerInRange(0, HIDDEN_STONES.length-1);
-  console.log("stone index is " + stoneIndex);
-  console.log("stone is " + HIDDEN_STONES[stoneIndex]);
-
-  //Let's get a random number index for an X value
-  var xIndex = rnd.integerInRange(1, NUM_X_CELLS);
-  console.log("xIndex is " + xIndex);
-  //Let's get a random number index for an Y value
-  var yIndex = rnd.integerInRange(1, NUM_Y_CELLS);
-  console.log("yIndex is " + yIndex);
-
-  //Retrieve the gemState and set it to the new name!
-  //gemStates is a Map that contains pairs of strings with the first one being a key
-  //getState.get('1:1') returns the state of the gem at 1:1
-  var retrieveGem = gemStates.get(xIndex + ":" + yIndex);
-  retrieveGem.gem = HIDDEN_STONES[stoneIndex];
+  for (i = 0; i < 25; i++) {
+    hideJewels2();
   }
-
 }
 
+function hideJewels2(){
+  
+  var triesToHide = 0;
+  var didHideStone = false;
+  do {
+  //Let's get a random number index for an X value
+    var xIndex = rnd.integerInRange(1, NUM_X_CELLS);
+    console.log("hj2:xIndex is " + xIndex);
+    //Let's get a random number index for an Y value
+    var yIndex = rnd.integerInRange(1, NUM_Y_CELLS);
+    console.log("hj2:yIndex is " + yIndex); 
+
+    //Retrieve the gemState and set it to the new name!
+    //gemStates is a Map that contains pairs of strings with the first one being a key
+    //getState.get('1:1') returns the state of the gem at 1:1
+    var retrieveGem = gemStates.get(xIndex + ":" + yIndex);
+
+     //Check if it's not the top stone.  If it is not the top stone it has to be a hidden stone
+    if (retrieveGem.gem === DEFAULT_STONE) {
+   
+      //Let's get a random number index to pick a stone name from the HIDDEN_STONES array
+      var stoneIndex = rnd.integerInRange(0, HIDDEN_STONES.length-1);
+      console.log("hj2:stone index is " + stoneIndex);
+      console.log("hj2:stone is " + HIDDEN_STONES[stoneIndex]);
+      retrieveGem.gem = HIDDEN_STONES[stoneIndex];
+      didHideStone = true;
+    
+     }
+    triesToHide++;
+  }
+  while (didHideStone == false && triesToHide < 1000);
+
+  console.log("hj2:Hid a stone after " + triesToHide + " tries.");
+}
 
 function generateGem(scene, x,y)
 {
@@ -102,7 +120,7 @@ function create()
       var currentState = {};
       currentState.x = x;
       currentState.y = y;
-      currentState.gem = 'pyramid';
+      currentState.gem = DEFAULT_STONE;
       gemStates.set(x+":"+y, currentState )
 
       allGems.push(newGem);
